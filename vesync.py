02ef19686346ab9a8c27ff3172b5ea9ef1ca5dfe
Marcello Feroce <ferocemarcello@gmail.com>
2023-07-27 14:25:30 +0200

    doing stuff

--- vesync.py
+++ vesync.py
@@ -1,12 +1,7 @@
-from pyvesync import VeSync
-import myfitnesspal
+from garminconnect import Garmin
 
 def get_data():
-    import garmin_fit_sdk
-    import requests
-    client = garmin_fit_sdk.GarminConnectAPIClient(username="ferocemarcello@gmail.com", password="")
-    activities = client.get_activities()
-    for activity in activities:
-        print(activity.name)
-
-    return ""
+    client = Garmin('ferocemarcello@gmail.com', 'password')
+    username = client.username
+    print(username)
+    return username
